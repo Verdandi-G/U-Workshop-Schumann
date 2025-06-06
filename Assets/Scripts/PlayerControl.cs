867a9ef698b2138db55286dd22051cd25f55837f
Verdandi <verdandi.dev1@gmail.com>
2025-06-06 19:08:46 +0200

    Moving Player

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    private CharacterController Figur;
+    private Vector3 MoveVector = Vector3.zero;
+    public float Speed = 5.0f;
+
     void Start()
     {
-        
+        Figur = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        
+        MoveVector.x = Input.GetAxis("Horizontal");
+        MoveVector.z = Input.GetAxis("Vertical");
+        Figur.Move(MoveVector * Time.deltaTime * Speed);
     }
 }
