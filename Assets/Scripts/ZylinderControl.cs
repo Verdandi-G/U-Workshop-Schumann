1084b755bcc837c4eaf5ac5b9f82e6001509f034
Verdandi <verdandi.dev1@gmail.com>
2025-06-08 19:41:13 +0200

    Walking Animation

--- Assets/Scripts/ZylinderControl.cs
+++ Assets/Scripts/ZylinderControl.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using System.Collections;
 
 public class ZylinderControl : MonoBehaviour
 {
     private CharacterController Figur;
+    private Animator anim;
     private Vector3 MoveVector = Vector3.zero;
-    public float Speed = 5.0f;
+    public float Speed = 10.0f;
+    private int isWalking;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Figur = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        isWalking = Animator.StringToHash("isWalking");
     }
 
     // Update is called once per frame
@@ -17,5 +22,13 @@ public class ZylinderControl : MonoBehaviour
         MoveVector.x = Input.GetAxis("Horizontal");
         MoveVector.z = Input.GetAxis("Vertical");
         Figur.Move(MoveVector * Time.deltaTime * Speed);
+        if (MoveVector.x != 0 || MoveVector.z != 0)
+        {
+            anim.SetBool(isWalking, true);
+        }
+        else
+        {
+            anim.SetBool(isWalking, false);
+        }
     }
 }
